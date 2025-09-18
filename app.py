a954f8352558c748f5fc491f23182ab5fd628e78
JdecookerFJ <152481730+JdecookerFJ@users.noreply.github.com>
2025-09-18 15:47:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 import pandas as pd
 import markovify
+import os
 
 app = Flask(__name__)
 
-# Quotes laden en Markov-model maken
 df = pd.read_csv("quotes.csv", sep=";")
 quotes = df["Quote"].dropna().astype(str).str.strip()
 model = markovify.NewlineText("\n".join(quotes), state_size=3)
@@ -22,4 +22,5 @@ def index():
     return render_template("index.html", quote=quote)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
